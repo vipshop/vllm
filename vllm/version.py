62d55a11cc10e3704786e799b6d43f8b40c07480
DefTruth <qiustudent_r@163.com>
2025-03-21 21:57:41 +0800

    [Misc][VIP] temp fix version as 0.8.2.dev
    Signed-off-by: DefTruth <qiustudent_r@163.com>

--- vllm/version.py
+++ vllm/version.py
@@ -2,15 +2,15 @@
 
 try:
     from ._version import __version__, __version_tuple__
-except Exception as e:
-    import warnings
+except Exception:
+    # import warnings
 
-    warnings.warn(f"Failed to read commit hash:\n{e}",
-                  RuntimeWarning,
-                  stacklevel=2)
+    # warnings.warn(f"Failed to read commit hash:\n{e}",
+    #               RuntimeWarning,
+    #               stacklevel=2)
 
     __version__ = "dev"
-    __version_tuple__ = (0, 0, __version__)
+    __version_tuple__ = (0, 8, __version__)
 
 
 def _prev_minor_version_was(version_str):
