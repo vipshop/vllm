14c61eb63fa6aaf154632d7656f10b879144e42d
DefTruth <31974251+DefTruth@users.noreply.github.com>
2025-03-22 16:47:46 +0800

    [Misc][VIP] Revert to vLLM offical version policy (#7)
    Signed-off-by: DefTruth <qiustudent_r@163.com>

--- vllm/version.py
+++ vllm/version.py
@@ -2,15 +2,15 @@
 
 try:
     from ._version import __version__, __version_tuple__
-except Exception:
-    # import warnings
+except Exception as e:
+    import warnings
 
-    # warnings.warn(f"Failed to read commit hash:\n{e}",
-    #               RuntimeWarning,
-    #               stacklevel=2)
+    warnings.warn(f"Failed to read commit hash:\n{e}",
+                  RuntimeWarning,
+                  stacklevel=2)
 
-    __version__ = "0.8.2.dev"
-    __version_tuple__ = (0, 8, 2, "dev")
+    __version__ = "dev"
+    __version_tuple__ = (0, 0, __version__)
 
 
 def _prev_minor_version_was(version_str):
